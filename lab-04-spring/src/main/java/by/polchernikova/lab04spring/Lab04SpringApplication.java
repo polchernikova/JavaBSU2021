6e4093411dd84ffe9e01373615020ee129c551ed
polchernikova <polchernikova@.gmail.com>
2021-11-20 20:30:03 +0300

    added missing files

---
+++ lab-04-spring/src/main/java/by/polchernikova/lab04spring/Lab04SpringApplication.java
@@ -0,0 +1,13 @@
+package by.polchernikova.lab04spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab04SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab04SpringApplication.class, args);
+    }
+
+}
